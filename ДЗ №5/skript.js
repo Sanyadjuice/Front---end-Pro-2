1576935500b4e675c965d3fd9324a2127453d440
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-03-31 16:44:04 +0300

    DZ №6

--- ДЗ №5/skript.js
+++ ДЗ №5/skript.js
@@ -1,23 +1,43 @@
-//Task1
+//Task #1
+//     Создайте пустой объект user.
+    const user = {};
+//     Добавьте свойство name со значением John.
+    user.name = 'John';
+//     Добавьте свойство surname со значением Smith.
+    user.surname = 'Smith';
+//     Измените значение свойства name на Pete.
+    user.name = 'Pete';
+//     Удалите свойство name из объекта.
+    delete user.name
 
-let admin;
+//     Task #2
+// Можно ли изменить объект, объявленный с помощью const?
+// Да
+// const user = {
+//
+//     name: «John»
+//
+// };
+//
+// // это будет работать?
+// нет, здесь не правильные кавычки
+// user.name = «Pete»;
+//
+//
+//
+// Task #3
+// У нас есть объект, в котором хранятся зарплаты нашей команды:
+//
+    let salaries = {
 
-let name;
+        John: 100,
 
-name = 'Джон';
+        Ann: 160,
 
-admin = name;
+        Pete: 130
 
-alert(admin);
-
-//Task2
-let ourPlanet;
-let currentUser;
-
-//Task3
-
-let a;
-
-a = '10' + 10;
-
-console.log(typeof(a));
+    }
+//
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum
+    let sum = salaries.John + salaries.Ann + salaries.Pete
+    console.log(sum)
