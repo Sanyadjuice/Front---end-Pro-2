183434c6b4eedf692aab71d2399a569b135ecdd0
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-03-31 20:49:11 +0300

    DZ №10

---
+++ ДЗ №10/skript.js
@@ -0,0 +1,54 @@
+// let a = +prompt("Введите данные 1", )
+// let b = +prompt("Введите данные 2", )
+// a === 0 ? console.log("Верно") : console.log("Неверно")
+// a > 0 ? console.log("Верно") : console.log("Неверно")
+// a < 0 ? console.log("Верно") : console.log("Неверно")
+// a >= 0 ? console.log("Верно") : console.log("Неверно")
+// a <= 0 ? console.log("Верно") : console.log("Неверно")
+// a !== 0 ? console.log("Верно") : console.log("Неверно")
+// a === "test" ? console.log("Верно") : console.log("Неверно")
+// a === "1" ? console.log("Верно") : console.log("Неверно")
+// 0 < a < 5 ? console.log("Верно") : console.log("Неверно")
+// if( a === 0 || a === 2 ){
+//     a += 7;
+//     alert (a);
+// }else{
+//     a /= 10;
+//     alert (a);
+// }
+//
+//
+// if( a <= 1 && b >=3 ){
+//     result = a + b;
+//     alert (result);
+// }else{
+//     result = a - b;
+//     alert (result);
+// }
+//
+// if (a <= 1 || b >= 6 && b < 14) {
+//     alert("Верно")
+// } else {
+//     alert("Неверно")
+// }
+
+let num = +prompt("Введите case", )
+let result
+switch (num) {
+    case 1:
+            result = "Зима";
+            alert(result);
+            break;
+    case 2:
+            result = "Весна"
+            alert(result)
+            break;
+    case 3:
+            result = "Лето"
+            alert(result)
+            break;
+    case 4:
+            result = "Осень"
+            alert(result)
+            break;
+}
---
+++ ДЗ №11/skript.js
@@ -0,0 +1,36 @@
+// Task# 1
+// Что вернут следующие строки кода?
+
+    // console.log( Number(’10’) + 1 ); // Uncaught SyntaxError: Invalid or unexpected token
+    // console.log(‘sub ’ + Number(false)); // Uncaught SyntaxError: Invalid or unexpected token
+    // console.log(16  *  ‘      91    ‘);  // Uncaught SyntaxError: Invalid or unexpected token
+    // console.log(true-70);  // -69
+    // console.log(Number(1 + String(1)) + 1); // 12
+
+
+// Task# 2
+// Напишите скрипт, который считает количество секунд в часе.
+   console.log(60 * 60)
+// Запросить у пользователя количество часов
+    const hour = prompt("Сколько часов?", )
+// Посчитать сколько секунд в этом количестве часов
+    console.log(60 * 60 * hour)
+// Записать вычесленное значение в переменную
+    let secondsInHour = 60 * 60 * hour
+// Вывести эту переменную пользователю во всплывающем окне
+    alert(secondsInHour)
+//
+// Task #3
+//
+// Переделайте приведенный код так, чтобы в нем использовались операции +=, -=, *=, /=, ++, —. Количество строк кода при этом не должно измениться. Код для переделки:
+//
+let num = 1;
+num += 12 ;
+num -= 14 ;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+
+//
