55e616066941410366b2861d880d502a5dee08cf
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-03-31 19:35:04 +0300

    DZ №9

---
+++ ДЗ №9/skript.js
@@ -0,0 +1,28 @@
+// Task #1
+
+const numFirst = prompt('Enter the number');
+console.log( numFirst , typeof numFirst );
+if( numFirst === null || numFirst.trim() === '' || isNaN( +numFirst ) ) {
+    alert('Error');
+} else {
+    console.log('Good');
+}
+
+// Task #2
+
+const numSecond = prompt('Enter the number');
+console.log ( numSecond, typeof numSecond );
+switch (true) {
+    case numSecond === null:
+        alert('You canceled an action!');
+        break;
+    case numSecond.trim() === '':
+        alert('Fill in an empty line!');
+        break;
+    case isNaN(+numSecond):
+        alert('Enter the number!');
+        break;
+    default:
+        alert('You entered a number!');
+        break;
+}
