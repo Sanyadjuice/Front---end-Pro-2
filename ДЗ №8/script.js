c361370cbd25393fd11944531d214f345e106ab5
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-04-02 10:14:26 +0300

    DZ №8.1

--- ДЗ №8/script.js
+++ ДЗ №8/script.js
@@ -1,11 +1,11 @@
 // Task# 1
 // Что вернут следующие строки кода?
 
-    // console.log( Number(’10’) + 1 ); // Uncaught SyntaxError: Invalid or unexpected token
-    // console.log(‘sub ’ + Number(false)); // Uncaught SyntaxError: Invalid or unexpected token
-    // console.log(16  *  ‘      91    ‘);  // Uncaught SyntaxError: Invalid or unexpected token
-    // console.log(true-70);  // -69
-    // console.log(Number(1 + String(1)) + 1); // 12
+    console.log( Number('10') + 1 ); // 11
+    console.log('sub' + Number(false)); // sub0
+    console.log(16  * '91');  // 1456
+    console.log(true-70);  // -69
+    console.log(Number(1 + String(1)) + 1); // 12
 
 
 // Task# 2
