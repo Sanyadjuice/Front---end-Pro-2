585b04267513c57ca6d58260ceceb3bb3ffab2cb
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-04-10 12:16:23 +0300

    DZ №16

--- ДЗ №16/script.js
+++ ДЗ №16/script.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// function pow (number, exponentiation = 1) {
-//
-//     if (number === null || exponentiation === null) {
-//         alert('Вы не ввели данные')
-//     } else if () {
-//         return alert('some error')
-//     } else  {
-//         result = Math.pow(number,exponentiation)
-//         return alert(result)
-//     }
-//
-//
-// }
-// let number = +prompt('Введите число которое нужно возвести в степень',);
-// let exponentiation = +prompt('Введите число которое нужно возвести в степень');
-//
-// pow(number);
+
+function pow (number, exponentiation = 1) {
+    if (number === 0 || isNaN(number) || exponentiation === 0 || isNaN(exponentiation)) {
+        return alert('some error')
+    } else {
+        return alert(Math.pow(number, exponentiation))
+    }
+}
+let number = +prompt('Введите число которое нужно возвести в степень',);
+
+let exponentiation = +prompt('Введите число которое нужно возвести в степень');
+
+
+pow(number, exponentiation) ;
+
+
