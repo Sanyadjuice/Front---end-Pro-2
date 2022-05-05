5b0197a9fddad4c7ec19601b816108882841951c
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-05-05 21:18:11 +0300

    DZ №26

--- ДЗ №26/script.js
+++ ДЗ №26/script.js
@@ -1,18 +1,23 @@
 'use strict';
 
-function randomArray() {
+function outputRandomly() {
     const arr = [];
-    for (let i = 0; arr.length < 100; i ++) {
+    function randomArray() {
         let randomNum = Math.floor(Math.random() * 100 + 1);
         if (!arr.includes(randomNum)) {
             arr.push(randomNum);
         }
+    }
+
+    for (let i = 0; arr.length < 100; i ++) {
+        randomArray()
     }
     return arr;
 
 
 }
 
-console.log(randomArray());
+outputRandomly();
+console.log(outputRandomly());
 
 
