d24fd64a797e50a2518e742be7cce008ccbef87f
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-05-13 16:33:21 +0300

    DZ №28

--- ДЗ №28/script.js
+++ ДЗ №28/script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+let calculator = {
+    read : function () {
+        this.a = +prompt("Введите первое число", )
+        this.b = +prompt("Введите второе число", )
+        if (isNaN(this.a && this.b)) {
+           return alert("Вы ввели не число")
+        }
+    },
+    sum : function () {
+        return this.a + this.b
+    },
+
+    mul : function () {
+        return this.a * this.b
+    }
+
+};
+
+calculator.read();
+if (isNaN(calculator.sum() && calculator.mul())) {
+    alert("Введите число")
+} else {
+    alert( calculator.sum() );
+    alert( calculator.mul() );
+}
+
+
