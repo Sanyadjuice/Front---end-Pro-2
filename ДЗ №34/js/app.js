3dafbab80db254000183376759616fc7a2a7e1e2
Sanya Bozhenko <sanya.djuice@ukr.net>
2022-06-01 10:12:47 +0300

    DZ №34

--- ДЗ №34/js/app.js
+++ ДЗ №34/js/app.js
@@ -9,17 +9,19 @@
     for (let i = 0; i < 100; i++) {
         if ((i + 1) % 10 === 0) {
             let td = document.createElement('td');
-            td.innerHTML = String(i + 1);
-            row.append(td);
+            td.innerHTML = `${i + 1}`;
             table.append(row);
+            row.append(td);
             row = document.createElement('tr');
         } else {
             const td = document.createElement('td');
-            td.innerHTML = String(i + 1);
+            td.innerHTML = `${i + 1}`;
             row.append(td);
+
         }
+
     }
     document.body.prepend(table);
-
+    table.style.margin = '0 auto'
 })();
 
